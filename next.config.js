658b300aaf9827b4b2a3b6073edf72272e23ea17
Diego Moraes <diegofrost96@gmail.com>
2022-12-16 20:52:40 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+module.exports = withPWA({
+  // next.js config
+})
